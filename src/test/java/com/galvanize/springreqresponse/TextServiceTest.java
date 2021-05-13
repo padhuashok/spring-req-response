ca2b9bdc025cccca5a5af6a2379c6ee8f5f22493
EdgarBarrientos <Edgar_O_Barrientos@outlook.com>
2021-05-13 19:43:56 -0400

    Create Second Test Case

--- src/test/java/com/galvanize/springreqresponse/TextServiceTest.java
+++ src/test/java/com/galvanize/springreqresponse/TextServiceTest.java
@@ -41,4 +41,32 @@ public class TextServiceTest {
                 .andExpect(content().string("ThisIsAThing"));
     }
 
+    /**
+     * Write an endpoint that maps to GET /redact. The endpoint should take:
+     *
+     * A parameter named original, which is a String
+     * Multiple parameters named badWord
+     * It should split the original String on spaces, and replace all badWords with *s of the same length.
+     *
+     * Examples
+     *
+     * If you send the following request:
+     *
+     * GET /redact?original=A little of this and a little of that&badWord=little&badWord=this
+     * You should get the following response:
+     *
+     * A ****** of **** and a ****** of that
+     * @throws Exception
+     */
+    @Test
+    public void testSplit() throws Exception {
+
+        RequestBuilder rq = MockMvcRequestBuilders.get("/redact").
+                queryParam("original","this_is_a_thing-with_many_letters").
+                queryParam("badWord","this,a,letters").
+                accept(MediaType.TEXT_PLAIN);
+        this.mvc.perform(rq).
+                andExpect(status().isOk())
+                .andExpect(content().string("****_is_*_thing-with_many_*******"));
+    }
 }
