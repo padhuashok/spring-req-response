1e541b188d5bca0b89b8841c157014fea6a3b46c
Padmashree Ashokkumar <padhu2192@gmail.com>
2021-05-13 19:24:19 -0400

    Settings files

---
+++ src/main/java/com/galvanize/springreqresponse/SpringReqResponseApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.springreqresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReqResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReqResponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/springreqresponse/SpringReqResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.springreqresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReqResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
